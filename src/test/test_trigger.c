105849f1732317036c1d26187a05cb4a76a58e21
Tobias Knopp <tobias@knoppweb.de>
2017-11-05 14:00:37 +0100

    add test script leading to a hang of the AXI bus

---
+++ src/test/test_trigger.c
@@ -0,0 +1,62 @@
+#include <stdint.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <inttypes.h>
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h> 
+#include <netinet/in.h>
+#include <pthread.h>
+#include <sched.h>
+
+#include "../lib/rp-daq-lib.h"
+
+int main () {
+  uint32_t wp, wp_old;
+
+  setMaster();
+  init();
+  setDACMode(DAC_MODE_RASTERIZED);
+  setWatchdogMode(WATCHDOG_OFF);  
+
+  setMasterTrigger(MASTER_TRIGGER_OFF);
+  setRAMWriterMode(ADC_MODE_TRIGGERED);
+
+  while(true)
+  {
+    setMasterTrigger(MASTER_TRIGGER_OFF);
+    usleep(40);
+    setMasterTrigger(MASTER_TRIGGER_ON);
+    usleep(40);
+    wp_old = 0; 
+
+    while(getTriggerStatus() == 0)
+    {
+      printf("Waiting for external trigger!"); 
+      usleep(40);
+    }
+
+    wp = getWritePointer();
+
+    uint32_t size = getWritePointerDistance(wp_old, wp)-1;
+
+    printf("____ %d %d %d \n", size, wp_old, wp);
+    if(size == 0) {
+      printf("Write Pointer remains zero!");
+      return 1;
+    }
+
+    usleep(40);    
+  } 
+  return 0;
+}
+
