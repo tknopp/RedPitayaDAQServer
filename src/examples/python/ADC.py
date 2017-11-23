79bb40959493779b0a5bcd4a74a65ef2768012e5
Jonas Beuke <Beuke@imt.uni-luebeck.de>
2017-11-23 14:30:10 +0100

    Move Python ADC example

--- src/examples/python/ADC.py
+++ src/examples/python/ADC.py
@@ -1,11 +1,14 @@
 # An example for using the server to acquire some data.
 # Connect OUT1 with IN1!
 
+import os
+import sys
 import math
 import time
 import matplotlib.pyplot as plt
 
 # Add the client object to the path
+sys.path.insert(0, os.path.join(os.getcwd(), '..', '..', 'client', 'python'))
 from redpitaya import RedPitaya
 
 # Connect to the Red Pitaya
