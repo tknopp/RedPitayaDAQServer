9af91af3431162fe0cc466581e38df804742452e
Tobias Knopp <tobias@knoppweb.de>
2018-08-20 07:31:45 +0200

    fixes

--- src/RedPitayaDAQServer.jl
+++ src/RedPitayaDAQServer.jl
@@ -5,6 +5,9 @@ module RedPitayaDAQServer
 using Sockets
 import Sockets: send, connect
 
+using Statistics
+using LinearAlgebra
+
 import Base: start, reset
 
 export RedPitaya, receive, query, stop, disconnect
@@ -85,7 +88,7 @@ function RedPitaya(host, port=5025)
   rp.samplesPerPeriod = samplesPerPeriod(rp)
   rp.periodsPerFrame = periodsPerFrame(rp)
 
-  finalizer(rp, d -> disconnect(d))
+  finalizer(d -> disconnect(d), rp)
   return rp
 end
 
