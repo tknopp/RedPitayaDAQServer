715a9c5bf96a6f153889eee8a9842951982f38c5
nHackel <nHackel@users.noreply.github.com>
2021-01-26 11:53:34 +0100

    Updated SlowDACTest2, now tracks performance too and switched to cluster, since this uses the new sample retrieval code

--- src/examples/julia/SlowDACTest2.jl
+++ src/examples/julia/SlowDACTest2.jl
@@ -1,7 +1,7 @@
 using RedPitayaDAQServer
 using PyPlot
 
-rp = RedPitaya("192.168.178.46")
+rp = RedPitayaCluster(["192.168.20.4"])
 
 dec = 64
 modulus = 4800
@@ -13,11 +13,10 @@ periods_per_frame = div(20, periods_per_step) # about 0.5 s frame length
 decimation(rp, dec)
 samplesPerPeriod(rp, samples_per_period)
 periodsPerFrame(rp, periods_per_frame)
-passPDMToFastDAC(rp, true)
-slowDACPeriodsPerFrame(rp, periods_per_frame)
-numSlowDACChan(rp, 1)
+passPDMToFastDAC(master(rp), true)
+numSlowDACChan(master(rp), 1)
 lut = collect(range(0,0.7,length=periods_per_frame))
-setSlowDACLUT(rp, lut)
+setSlowDACLUT(master(rp), lut)
 
 modeDAC(rp, "STANDARD")
 frequencyDAC(rp, 1, 1, base_frequency / modulus)
@@ -29,22 +28,23 @@ ramWriterMode(rp, "TRIGGERED")
 triggerMode(rp, "INTERNAL")
 
 numTrials = 10
+rpInfo = RPInfo()
 
-signals = zeros(samples_per_period*periods_per_frame, 2, numTrials)
-
+signals = zeros(samples_per_period*periods_per_frame * 4, 2, numTrials)
 for i=1:numTrials
     @info "measure trial $i"
     masterTrigger(rp, false)
-    startADC(rp, 0)
+    startADC(rp)
     masterTrigger(rp, true)
     local currFr = enableSlowDAC(rp, true, 1, 0.5, 1.0)
-    data = readData(rp, currFr, 1)
+    data = readFrames(rp, currFr - 2, 4, rpInfo = rpInfo)
+    #data = readData(rp, currFr - 2, 4)
     signals[:,1,i] .= vec(data[:,1,:,:])
     signals[:,2,i] .= vec(data[:,2,:,:])
     stopADC(rp)
 end
 
-figure(1)
+fig = figure(1)
 clf()
 subplot(2,1,1)
 for i=1:numTrials
