f9ab65aa5343d40d47d545dc6bea21384208c73c
nHackel <nHackel@users.noreply.github.com>
2021-12-06 14:13:43 +0100

    Added client functions for calibDACOffset

--- src/client/julia/src/EEPROM.jl
+++ src/client/julia/src/EEPROM.jl
@@ -1,4 +1,4 @@
-export eepromField, resetEEPROM
+export eepromField, resetEEPROM, calibDACOffset
 
 function eepromField(rp::RedPitaya, field::AbstractString, val::Integer)
   send(rp, "RP:CALib $field,$val")
@@ -6,3 +6,12 @@ end
 eepromField(rp::RedPitaya, field::AbstractString) = query(rp, "RP:CALib? $field", Int64)
 
 resetEEPROM(rp::RedPitaya) = send(rp, "RP:CALib:RESet")
+
+function calibDACOffset(rp::RedPitaya, channel::Integer, val)
+  if val > 1.0
+    error("$val is larger than 1.0 V!")
+  end
+  command = string("RP:CALib:DAC:CH", Int(channel) - 1, ":OFF $(Float32(val))")
+  return send(rp, command)
+end
+calibDACOffset(rp::RedPitaya, channel::Integer) = query(rp, string("RP:CALib:DAC:CH", Int(channel) - 1, ":OFF?"), Float64)
