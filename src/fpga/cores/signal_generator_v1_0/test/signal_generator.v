c0a0a6b3da6d9be370a5c9725603ce249696a502
Jonas Schumacher <jonas.beuke@gmx.de>
2022-01-28 22:29:15 +0100

    Clock all conditions

--- src/fpga/cores/signal_generator_v1_0/test/signal_generator.v
+++ src/fpga/cores/signal_generator_v1_0/test/signal_generator.v
@@ -22,9 +22,8 @@ module signal_generator #
 	reg [AXIS_TDATA_WIDTH-1:0] dac_out_temp_2;
 	reg [AXIS_TDATA_WIDTH-1:0] dac_out_temp_3;
 	reg [AXIS_TDATA_WIDTH-1:0] dac_out_temp_4;
-	reg [AXIS_TDATA_WIDTH-1:0] dac_out2;
-    reg [AXIS_TDATA_WIDTH-1:0] dac_out_temp2;
     reg signed [DAC_WIDTH-1:0] phase;
+	reg signed [DAC_WIDTH-1:0] phase_delayed;
     reg signed [15:0] A, AIncrement;
 	
 	wire [4-1:0] trapezoidCond;
@@ -35,8 +34,13 @@ module signal_generator #
 	assign trapezoidCond[2] = (phase > A);
 	assign trapezoidCond[3] = (phase >= 8191-A);
 	
-	wire justSwitchedCond;
-	assign justSwitchedCond = (lastTrapezoidCond != trapezoidCond);
+	reg justChangedCond;
+	
+	// Reference
+	reg [AXIS_TDATA_WIDTH-1:0] dac_out2;
+    reg [AXIS_TDATA_WIDTH-1:0] dac_out_temp2;
+	reg [AXIS_TDATA_WIDTH-1:0] dac_out_temp2_delayed;
+	reg [AXIS_TDATA_WIDTH-1:0] dac_out_temp2_delayed2;
 	
     always @(posedge clk)
     begin
@@ -49,90 +53,84 @@ module signal_generator #
 			dac_out_temp_3 <= 8191;
 			dac_out_temp_4 <= 0;
             phase <= -8191;
+			phase_delayed <= -8191;
             A <= 80;
             AIncrement <= 100; // 2*8191 / (2*A);
 			signal_type <= 1;
 			lastTrapezoidCond <= 4'b0000;
+			justChangedCond <= 0;
         end
         else
         begin
             phase <= phase+10;
+			phase_delayed <= phase;
 			
-			if (justSwitchedCond == 1)
+			if (lastTrapezoidCond != trapezoidCond)
 			begin
+				justChangedCond <= 1;
 				lastTrapezoidCond <= trapezoidCond;
+			end
+			
+			if (justChangedCond == 1)
+			begin
+				justChangedCond <= 0;
 			end
 
             if (signal_type == 1) // Trapezoid
             begin
-				case (trapezoidCond)
-					4'b0000 : begin
-						$display("0000 phase = %d, temp = %d", phase, dac_out_temp_0);
-						dac_out_temp_0 <= -8191-phase;
-						
-						if (justSwitchedCond == 1)
-						begin
-							dac_out <= AIncrement*dac_out_temp_4;
-						end
-						else
-						begin
-							dac_out <= AIncrement*dac_out_temp_0;
-						end
+				case ({justChangedCond, lastTrapezoidCond})
+					5'b10000 : begin
+						$display("10000 phase = %d, temp = %d", phase_delayed, dac_out_temp_0);
+						dac_out_temp_0 <= -8191-phase_delayed;
+						dac_out <= AIncrement*dac_out_temp_4;
 					end
-					4'b0001 : begin
-						$display("0001 phase = %d, temp = %d", phase, dac_out_temp_1);
-						dac_out_temp_1 <= -8191;
-						
-						if (justSwitchedCond == 1)
-						begin
-							dac_out <= AIncrement*dac_out_temp_0;
-						end
-						else
-						begin
-							dac_out <= dac_out_temp_1;
-						end
+					5'b00000 : begin
+						$display("00000 phase = %d, temp = %d", phase_delayed, dac_out_temp_0);
+						dac_out_temp_0 <= -8191-phase_delayed;
+						dac_out <= AIncrement*dac_out_temp_0;
 					end
-					4'b0011 : begin
-						$display("0011 phase = %d, temp = %d", phase, dac_out_temp_2);
-						dac_out_temp_2 <= phase;
-						
-						if (justSwitchedCond == 1)
-						begin
-							dac_out <= dac_out_temp_1;
-						end
-						else
-						begin
-							dac_out <= AIncrement*dac_out_temp_2;
-						end
+					5'b10001 : begin
+						$display("10001 phase = %d, temp = %d", phase_delayed, dac_out_temp_1);
+						//dac_out_temp_1 <= -8191;
+						dac_out <= AIncrement*dac_out_temp_0;
 					end
-					4'b0111 : begin
-						$display("0111 phase = %d, temp = %d", phase, dac_out_temp_3);
-						dac_out_temp_3 <= 8191;
-						
-						if (justSwitchedCond == 1)
-						begin
-							dac_out <= AIncrement*dac_out_temp_2;
-						end
-						else
-						begin
-							dac_out <= dac_out_temp_3;
-						end
+					5'b00001 : begin
+						$display("00001 phase = %d, temp = %d", phase_delayed, dac_out_temp_1);
+						//dac_out_temp_1 <= -8191;
+						dac_out <= dac_out_temp_1;
 					end
-					4'b1111 : begin
-						$display("1111 phase = %d, temp = %d", phase, dac_out_temp_4);
-						dac_out_temp_4 <= 8191-phase;
-						
-						if (justSwitchedCond == 1)
-						begin
-							dac_out <= dac_out_temp_3;
-						end
-						else
-						begin
-							dac_out <= AIncrement*dac_out_temp_4;
-						end
+					5'b10011 : begin
+						$display("0011 phase = %d, temp = %d", phase_delayed, dac_out_temp_2);
+						dac_out_temp_2 <= phase_delayed;
+						dac_out <= dac_out_temp_1;
+					end
+					5'b00011 : begin
+						$display("0011 phase = %d, temp = %d", phase_delayed, dac_out_temp_2);
+						dac_out_temp_2 <= phase_delayed;
+						dac_out <= AIncrement*dac_out_temp_2;
+					end
+					5'b10111 : begin
+						$display("0111 phase = %d, temp = %d", phase_delayed, dac_out_temp_3);
+						//dac_out_temp_3 <= 8191;
+						dac_out <= AIncrement*dac_out_temp_2;
+					end
+					5'b00111 : begin
+						$display("0111 phase = %d, temp = %d", phase_delayed, dac_out_temp_3);
+						//dac_out_temp_3 <= 8191;
+						dac_out <= dac_out_temp_3;
+					end
+					5'b11111 : begin
+						$display("1111 phase = %d, temp = %d", phase_delayed, dac_out_temp_4);
+						dac_out_temp_4 <= 8191-phase_delayed;
+						dac_out <= dac_out_temp_3;
+					end
+					5'b01111 : begin
+						$display("1111 phase = %d, temp = %d", phase_delayed, dac_out_temp_4);
+						dac_out_temp_4 <= 8191-phase_delayed;
+						dac_out <= AIncrement*dac_out_temp_4;
 					end
 					default : begin
-						$display("default phase = %d", phase);
+						$display("default phase = %d", phase_delayed);
 						dac_out <= 0;
 					end
 				endcase
@@ -158,7 +156,9 @@ module signal_generator #
                     dac_out_temp2 <= AIncrement*(8191-phase);
                 end
                 
-                dac_out2 <= dac_out_temp2;
+				dac_out_temp2_delayed <= dac_out_temp2;
+				dac_out_temp2_delayed2 <= dac_out_temp2_delayed;
+                dac_out2 <= dac_out_temp2_delayed;
 			end
             else if (signal_type == 2) // Triangle
             begin
@@ -180,7 +180,8 @@ module signal_generator #
             if (signal_type == 3) // Sawtooth
             begin
                 dac_out_temp_0 <= phase;
-                dac_out <= dac_out_temp_0;
+				dac_out_temp2_delayed <= dac_out_temp_0;
+                dac_out <= dac_out_temp2_delayed;
             end
        end
     end
