87f28e8c67b4587cc8ab076a8a3734cb2f0cfa1e
nHackel <nHackel@users.noreply.github.com>
2022-04-12 10:06:50 +0000

    Change signal composition logic

--- src/fpga/hdl/signal_composer.v
+++ src/fpga/hdl/signal_composer.v
@@ -2,37 +2,67 @@
 
 module signal_composer(
     input clk,
-    input [15:0] wave0,
-    input [15:0] wave1,
-    input [15:0] wave2,
-    input [15:0] wave3,
+    input signed [15:0] wave0,
+    input signed [15:0] wave1,
+    input signed [15:0] wave2,
+    input signed [15:0] wave3,
     input valid0,
     input valid1,
     input valid2,
     input valid3,
-    input [15:0] offset,
-    input [15:0] seq,
+    input signed [15:0] offset,
+    input signed [15:0] seq,
     input dyn_offset_disable,
     input disable_dac,
     output signal_valid,
-    output [15:0] signal_out
+    output signed [15:0] signal_out
     );
 
-reg [15:0] signal_int = 0;
+reg signed [15:0] signal_int = 0;
+reg signed [15:0] signal_temp0 = 0;
+reg signed [15:0] signal_temp1 = 0;
+reg signed [15:0] signal_temp2 = 0;
+reg signed [15:0] signal_temp3 = 0;
+reg signed [15:0] signal_temp4 = 0;
+reg signed [15:0] signal_temp5 = 0;
 reg valid_int = 0;
+reg valid_temp0 = 0;
+reg valid_temp1 = 0;
 
 always @(posedge clk)
 begin
-    signal_int <= 0;
-    if (~disable_dac)
+    signal_temp0 <= wave0 + wave1;
+    signal_temp1 <= wave2 + wave3;
+    signal_temp2 <= seq + offset;
+    valid_temp0 <= valid0 & valid1;
+    valid_temp1 <= valid2 & valid3;
+end
+
+always @*
+begin
+    signal_temp3 <= signal_temp0 + signal_temp1;
+    if (~dyn_offset_disable)
     begin
-        signal_int <= signal_int + wave0 + wave1 + wave2 + wave3;
-        if (~dyn_offset_disable)
-        begin
-            signal_int <= signal_int + seq + offset;
-        end
+        signal_temp4 <= signal_temp2; 
+    end else begin
+        signal_temp4 <= 0;
     end
-    valid_int = valid0 & valid1 & valid2 & valid3;
+end
+
+always @*
+begin
+    if  (~disable_dac)
+    begin
+        signal_temp5 <= signal_temp3 + signal_temp4;
+    end else begin
+        signal_temp5 <= 0;
+    end
+end
+
+always @* 
+begin
+    signal_int <= signal_temp5;
+    valid_int <= valid_temp0 & valid_temp1;
 end
 
 assign signal_valid = valid_int;
