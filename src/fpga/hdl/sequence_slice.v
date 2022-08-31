436b4d5299e3caec7d41d4df5f46be7c62f2faae
nHackel <nHackel@users.noreply.github.com>
2022-08-31 15:08:25 +0000

    Update sequence slice

--- src/fpga/hdl/sequence_slice.v
+++ src/fpga/hdl/sequence_slice.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module sequence_slice(
-    input[63:0] seq_data,
+    input[127:0] seq_data,
     output signed [15:0] dac_value_0,
     output signed [15:0] dac_value_1,
     output [10:0] pdm_value_0,
@@ -14,18 +14,18 @@ module sequence_slice(
     );
 
 // Values
-assign pdm_value_0[10:0] = seq_data[10:0];
-assign pdm_value_1[10:0] = seq_data[26:16];
-assign pdm_value_2[10:0] = seq_data[42:32];
-assign pdm_value_3[10:0] = seq_data[58:48];
-
 assign dac_value_0[15:0] = {{2{seq_data[13]}}, seq_data[13:0]};
 assign dac_value_1[15:0] = {{2{seq_data[31]}}, seq_data[29:16]};
 
+assign pdm_value_0[10:0] = seq_data[42:32];
+assign pdm_value_1[10:0] = seq_data[58:48];
+assign pdm_value_2[10:0] = seq_data[74:64];
+assign pdm_value_3[10:0] = seq_data[90:80];
+
 // Flags
-assign enable_dac[1:0] = seq_data[61:60];
-assign enable_pdm[3:0] = seq_data[63:60];
-assign enable_dac_ramp_down[0] = seq_data[46];
-assign enable_dac_ramp_down[1] = seq_data[47];
+assign enable_dac[1:0] = seq_data[97:96];
+assign enable_pdm[3:0] = seq_data[101:98];
+assign enable_dac_ramp_down[0] = seq_data[112];
+assign enable_dac_ramp_down[1] = seq_data[113];
 
 endmodule
