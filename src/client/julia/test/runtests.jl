60837b5c28de9c72397e1f90929b6dbd10e87734
Jonas Schumacher <github735@jonasschumacher.de>
2023-09-01 15:52:47 +0200

    Basic infrastructure

--- src/client/julia/test/runtests.jl
+++ src/client/julia/test/runtests.jl
@@ -1,9 +1,32 @@
 using RedPitayaDAQServer
 using Test
 using Aqua
+using Sockets
 
 @testset "RedPitayaDAQServer" begin
   @testset "Aqua" begin
     Aqua.test_all(RedPitayaDAQServer)
+  end
+
+  @testset "SingleRP" begin
+    if !isfile("config.jl")
+      @info "No config given for hardware-based tests. The tests will be skipped. If you want to run hardware-based tests, please create a file named `config.jl` in the `test` folder. The file should contain a variable named `singleIP` with a string giving the IP of the device under test."
+    else
+      include("config.jl")
+
+      if @isdefined singleIP
+        port = 5025
+        try
+          sock = Sockets.connect(singleIP, port)
+          close(sock)
+
+          include("single.jl")
+        catch e
+          @warn "No connection can be established to RP at `$singleIP:$port` for hardware-based tests. The tests will be skipped."
+        end
+      else
+        @info "No IP for a single RP was given by specifying `singleIP` for hardware-based tests. The tests will be skipped."
+      end
+    end
   end
 end
---
+++ src/client/julia/test/single.jl
@@ -0,0 +1,32 @@
+rp = RedPitaya(singleIP)
+
+@testset "Acquisition.jl" begin
+  
+end
+
+@testset "ADC.jl" begin
+  decimation!(rp, 8)
+  @test decimation(rp) == 8
+  decimation!(rp, 16)
+  @test decimation(rp) == 16
+
+  @test numChan(rp) == 2 # A single RP always has two ADC Channels
+
+  samplesPerPeriod!(rp, 42)
+  @test samplesPerPeriod(rp) == 42
+  samplesPerPeriod!(rp, 625)
+  @test samplesPerPeriod(rp) == 625
+
+
+  periodsPerFrame!(rp, 42)
+  @test samplesPerPeriod(rp) == 42
+  periodsPerFrame!(rp, 1)
+  @test periodsPerFrame(rp) == 1
+
+  @test currentFrame(rp) == 0 # The aquisition is not running
+  @test currentPeriod(rp) == 0 # The aquisition is not running
+  @test currentWP(rp) == 0 # The aquisition is not running
+
+  #TODO: Cont' here
+
+end
