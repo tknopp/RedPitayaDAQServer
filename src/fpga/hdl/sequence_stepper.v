b79123c0d24e173321b1cab3ca144b0c3ff86d51
rp_local <n.a>
2024-11-29 13:07:08 +0000

    Init test of step counting with writepointer lowest bit

--- src/fpga/hdl/sequence_stepper.v
+++ src/fpga/hdl/sequence_stepper.v
@@ -1,36 +1,45 @@
 `timescale 1ns / 1ps
 
 module sequence_stepper(
-    input [63:0] writepointer,
+    input [63:0] writePointer,
     input [31:0] stepSize,
     input clk,
     input aresetn,
-    output [31:0] step_counter 
+    output [31:0] seq_counter
 );
 
-reg [63:0] end_of_step, next_end_of_step;
-reg [31:0] step_counter_reg, step_counter_next;
+reg [31:0] stepSize_int;
+reg [31:0] seq_counter_state;
+reg [31:0] sample_counter;
+reg wp_prev, wp_next;
 
 always @(posedge clk)
 begin
+    stepSize_int <= stepSize;
+    wp_next <= writePointer[0];
     if (~aresetn) begin
-        step_counter_reg <= 0;
-        step_counter_next <= 0;
-        end_of_step <= stepSize;
-        next_end_of_step <= stepSize;
+        seq_counter_state <= 0;
+        sample_counter <= stepSize_int;
+        wp_prev <= 0;
     end else begin
-        step_counter_reg <= step_counter_next;
-        end_of_step <= next_end_of_step;
-        if (end_of_step < writepointer) begin
-            next_end_of_step <= end_of_step + stepSize;
-            step_counter_next <= step_counter_reg + 1;
+        wp_prev <= wp_next;
+        if (wp_prev != wp_next) begin
+        
+            if (sample_counter == 0) begin
+                sample_counter <= stepSize_int;
+                seq_counter_state <= seq_counter_state + 1;
+            end else begin
+                sample_counter <= sample_counter - 1;
+                seq_counter_state <= seq_counter_state;
+            end
+        
         end else begin
-            next_end_of_step <= next_end_of_step;
-            step_counter_next <= step_counter_reg;
+            sample_counter <= sample_counter;
+            seq_counter_state <= seq_counter_state;
         end
     end
 end
 
-assign step_counter = step_counter_reg;
+assign seq_counter = seq_counter_state;
 
 endmodule
